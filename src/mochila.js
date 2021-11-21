fc35f985ccb1e8f3ffe8d97bb4d3147d9422b65b
Felipe Hsu <hsufelipe@gmail.com>
2021-11-21 12:37:42 -0300

    Feat: Added another example

--- src/mochila.js
+++ src/mochila.js
@@ -1,24 +1,17 @@
 module.exports = (capacidate, itens) => {
-    // Para a solução ótima requer que a entrada das moedas 
-    // esteja ordenada de forma ascendente. Caso contrário, 
-    // não retornará o resultado ótimo.
+  // Para a solução ótima requer que a entrada das moedas
+  // esteja ordenada de forma ascendente. Caso contrário,
+  // não retornará o resultado ótimo.
 
-    let load = 0;
-    const output = []
-    for (let i = 0; load < capacidate && i < itens.length; i++) {
-        let fração = 0
-        if(itens[i].peso <= capacidate - load) {
-            fração = itens[i].peso
-            output.push(itens[i]);
-        }
-        else {
-            fração = (capacidate - load)/itens[i].peso
-            output.push({
-                valor: fração * itens[i].valor,
-                peso: itens[i].peso,
-            })
-        }
-        load += fração
+  let peso = 0;
+  let valor = 0;
+
+  for (let i = 0; peso < capacidate && i < itens.length; i++) {
+    let quantidade = parseInt((capacidate - peso) / itens[i].peso);
+    if (quantidade >= 1) {
+      peso += itens[i].peso * quantidade;
+      valor += itens[i].valor * quantidade;
     }
-    return output;
-}
+  }
+  return valor;
+};
--- test/mochila.test.js
+++ test/mochila.test.js
@@ -1,69 +1,43 @@
-const mochilaGulosa = require('../src/mochila')
+const mochilaGulosa = require("../src/mochila");
 
-describe('Problema da mochila - Fracionada', () =>  {
-    it('Caso entrada ordenada ascendente - Resultado esperado 240', () => {
-        const arr = [
-            {
-                valor: 60,
-                peso: 10
-            },
-            {
-                valor: 100,
-                peso: 20
-            },
-            {
-                valor: 120,
-                peso: 30
-            },
-        ]
-        const capacidate = 50
-        const result = mochilaGulosa(capacidate, arr)
-        const sum = result.reduce((a,b) => a+b.valor, 0)
-        expect(result).toEqual([
-            {
-                valor: 60,
-                peso: 10
-            },
-            {
-                valor: 100,
-                peso: 20
-            },
-            {
-                valor: 80,
-                peso: 30
-            },
-        ])
-        expect(sum).toEqual(240)
-    })
+describe("Problema da mochila - Fracionada", () => {
+  it("Caso entrada ordenada ascendente - Resultado esperado 240", () => {
+    const arr = [
+      {
+        valor: 100,
+        peso: 60,
+      },
+      {
+        valor: 60,
+        peso: 40,
+      },
+      {
+        valor: 20,
+        peso: 20,
+      },
+    ];
+    const capacidate = 5000;
+    const result = mochilaGulosa(capacidate, arr);
+    expect(result).toEqual(8320);
+  });
 
-    it('Caso entrada ordenada decrescente - Resultado esperado 220', () => {
-        const arr = [
-            {
-                valor: 120,
-                peso: 30
-            },
-            {
-                valor: 100,
-                peso: 20
-            },
-            {
-                valor: 60,
-                peso: 10
-            },
-        ]
-        const capacidate = 50
-        const result = mochilaGulosa(capacidate, arr)
-        const sum = result.reduce((a,b) => a+b.valor, 0)
-        expect(result).toEqual([
-            {
-                valor: 120,
-                peso: 30
-            },
-            {
-                valor: 100,
-                peso: 20
-            },
-        ])
-        expect(sum).toEqual(220)
-    })
-})
+  it("Caso entrada ordenada decrescente - Resultado esperado 220", () => {
+    const arr = [
+      {
+        valor: 60,
+        peso: 40,
+      },
+      {
+        valor: 100,
+        peso: 60,
+      },
+      {
+        valor: 20,
+        peso: 20,
+      },
+    ];
+    const capacidate = 5000;
+    const result = mochilaGulosa(capacidate, arr);
+    expect(result).toEqual(7500);
+  });
+});
